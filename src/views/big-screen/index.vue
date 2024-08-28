72f28688740339772cc89dac11fbef3ab107cf5c
hhfb8848 <sjh6690661@163.com>
2024-08-28 17:46:32 +0800

    IOT:数据大屏头部

--- src/views/big-screen/index.vue
+++ src/views/big-screen/index.vue
@@ -1,41 +1,31 @@
 <script setup lang="ts">
+import autofit from "autofit.js";
+import { onUnmounted } from "vue";
 import { useRouter } from "vue-router";
-
+import Header from "./component/Header.vue";
 defineOptions({
-  name: "Empty"
+  name: "BigScreen"
 });
-
+autofit.init();
+onUnmounted(() => {
+  autofit.off();
+});
 const router = useRouter();
 </script>
 
 <template>
-  <div class="w-full h-full text-center">
-    <h1>业务内容模块</h1>
-    <p>使用场景：需要外嵌平台某个页面，不需要展示菜单导航以及额外模块</p>
-    <div class="back" title="返回上一页" @click="router.go(-1)">
-      <back class="w-[80px] h-[80px]" />
-    </div>
+  <div class="big-screen-wrapper">
+    <Header />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.back {
-  position: relative;
-  top: 50%;
-  left: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 200px;
-  height: 200px;
-  cursor: pointer;
-  background: rgb(138 150 160 / 8%);
-  border-radius: 18px;
-  transform: translate(-50%, -50%);
-
-  &:hover {
-    background: rgb(138 150 160 / 20%);
-    transition: background 0.6s;
-  }
+.big-screen-wrapper {
+  width: 100%;
+  height: 100%;
+  background-image: url("@/assets/big-screen/pageBg.png");
+  background-size: cover;
+  background-position: center center;
+  padding: 20px;
 }
 </style>
